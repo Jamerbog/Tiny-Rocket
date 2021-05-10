cfb64b67ab4f54a711b753e3a7758d4922b353db
James McLeod (Student) <152914@halesowen.ac.uk>
2021-05-10 16:09:26 +0100

    began work on tutorial leve;

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -7,21 +7,29 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] GameObject rocket;
     Rigidbody rb;
 
-
+    float rocketZ;
     float rocketY;
     Transform rocketTransform;
     void Update()
     {
         rb = gameObject.GetComponent<Rigidbody>();
         rocketTransform = rocket.GetComponent<Transform>();
-        if (rocketTransform.position.z > 0f)
+
+
+        
+
+
+        if ((rocketTransform.position.y > 27f) || (rocketTransform.position.z > 0f) && !(rocketTransform.position.y > 27f))
         {
-            transform.position = (rocketTransform.position + new Vector3(80f, 15f));
-        }
-        else
-        {
-            rocketY = rocketTransform.position.y + 15f;
-            transform.position = new Vector3(80f, rocketY);
+            if (rocketTransform.position.z > 0f)
+            {
+                transform.position = (rocketTransform.position + new Vector3(80f, 15f));
+            }
+            else
+            {
+                rocketY = rocketTransform.position.y + 15f;
+                transform.position = new Vector3(80f, rocketY);
+            }
         }
     }
 }
