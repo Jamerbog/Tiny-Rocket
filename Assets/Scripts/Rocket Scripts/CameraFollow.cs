a0fde253d75360fb11f014ca053de17faf335720
Jamerbog <jamesmcleod.work@gmail.com>
2021-05-11 22:41:02 +0100

    Added a soundtrack to the tutorial level

--- Assets/Scripts/Rocket Scripts/CameraFollow.cs
+++ Assets/Scripts/Rocket Scripts/CameraFollow.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CameraFollow : MonoBehaviour
 {
@@ -9,26 +10,53 @@ public class CameraFollow : MonoBehaviour
 
     float rocketZ;
     float rocketY;
+
+    Scene scene;
+    int sceneInt;
+
     Transform rocketTransform;
+
+    private void Start()
+    {
+        scene = SceneManager.GetActiveScene();
+        sceneInt = scene.buildIndex;
+    }
+
     void Update()
     {
         rb = gameObject.GetComponent<Rigidbody>();
         rocketTransform = rocket.GetComponent<Transform>();
 
-
-        
-
-
-        if ((rocketTransform.position.y > 27f) || (rocketTransform.position.z > 0f) && !(rocketTransform.position.y > 27f))
+        if (sceneInt == 1)
         {
-            if (rocketTransform.position.z > 0f)
-            {
+            tutorialSettings();
+        }
+        else
+        {
+            standardSettings();
+        }
+
+        void tutorialSettings()
+        {
+            //if (rocketTransform.position.y > 27f)
+           // {
                 transform.position = (rocketTransform.position + new Vector3(80f, 15f));
-            }
-            else
+            //}
+        }
+
+        void standardSettings()
+        {
+            if ((rocketTransform.position.y > 27f) || (rocketTransform.position.z > 0f) && !(rocketTransform.position.y > 27f))
             {
-                rocketY = rocketTransform.position.y + 15f;
-                transform.position = new Vector3(80f, rocketY);
+                if (rocketTransform.position.z > 0f)
+                {
+                    transform.position = (rocketTransform.position + new Vector3(80f, 15f));
+                }
+                else
+                {
+                    rocketY = rocketTransform.position.y + 15f;
+                    transform.position = new Vector3(80f, rocketY);
+                }
             }
         }
     }
