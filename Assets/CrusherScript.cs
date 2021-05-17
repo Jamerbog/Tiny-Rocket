497270a2e857fe51e5b6844867d0324c165399a6
James McLeod (Student) <152914@halesowen.ac.uk>
2021-05-17 15:17:11 +0100

    finished level 1

--- Assets/CrusherScript.cs
+++ Assets/CrusherScript.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class CrusherScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    AudioSource audioSource;
+    [SerializeField] AudioClip crusherSound;
+    [SerializeField] GameObject crusher;
+    [SerializeField] float laserFrequency = 10f;
+    [SerializeField] float laserTime;
+    [SerializeField] float wait;
+    Animator animator;
 
-    // Update is called once per frame
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
+    }
     void Update()
     {
-        
+        if (laserTime <= 0f)
+        {
+            wait += 1f;
+            enableLaser();
+            laserTime += laserFrequency;
+
+        }
+
+        else if (laserTime > 0f && wait <= 0f)
+        {
+            disableLaser();
+            laserTime -= Time.deltaTime;
+        }
+
+        if (wait >= 0f)
+        {
+            wait -= Time.deltaTime;
+        }
+    }
+
+    void enableLaser()
+    {
+        animator.SetBool("triggerCrush", true);
+        audioSource.PlayOneShot(crusherSound);
+    }
+
+    void disableLaser()
+    {
+        animator.SetBool("triggerCrush", false);
+        print("do nothing");
     }
 }
