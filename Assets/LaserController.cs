62be1366173119f692f118bae06705211567da02
James McLeod (Student) <152914@halesowen.ac.uk>
2021-05-17 12:29:44 +0100

    added huge spinning fan and lasers to avoid

---
+++ Assets/LaserController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserController : MonoBehaviour
+{
+    AudioSource audioSource;
+    [SerializeField] AudioClip electricalBuzz;
+    [SerializeField] GameObject laserBeam;
+    [SerializeField] float laserFrequency = 10f;
+    [SerializeField] float laserTime;
+    [SerializeField] float wait;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    void Update()
+    {
+        if (laserTime <= 0f)
+        {
+            wait += 1f;
+            enableLaser();
+            laserTime += laserFrequency;
+
+        }
+
+        else if (laserTime > 0f && wait <= 0f)
+        {
+            disableLaser();
+            laserTime -= Time.deltaTime;
+        }
+
+        if (wait >= 0f)
+        {
+            wait -= Time.deltaTime;
+        }
+    }
+
+    void enableLaser()
+    {
+        laserBeam.SetActive(true);
+        audioSource.PlayOneShot(electricalBuzz);
+    }
+
+    void disableLaser()
+    {
+        laserBeam.SetActive(false);
+    }
+}
