1436cc10ba25462bd281b62efcd5fc245b0dd395
Jamerbog <jamesmcleod.work@gmail.com>
2021-05-20 13:07:44 +0100

    added music and some obstacles to level 2

---
+++ Assets/Scripts/playerDetector.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerDetector : MonoBehaviour
+{
+    [SerializeField] GameObject anchor;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            anchor.SetActive(true);
+        }
+    }
+}
